6ea57fe4cf3e9722bfafa66968350b844a7744ba
Alfredo Gallegos <j.gallegos.chavarria@accenture.com>
2016-12-25 21:44:05 -0600

    random grid behavior implemented and removes useless attributes from css

--- js/grid.js
+++ js/grid.js
@@ -1,13 +1,20 @@
 //grid.js
 //alfredo gallegos, 2016
 
+var square_size = 30;
+var grid_size = 900;
+
 getRandomColor = function() {
     COLORS = ["cyan", "magenta", "yellow"];
     return COLORS[Math.floor(Math.random() * COLORS.length)];
 }
 
 canvas = document.getElementById("grid");
-context = canvas.getContext("2d");
 
-context.fillStyle = getRandomColor();
-context.fillRect(10, 10, 100, 100);
+for(var v_offset = 0; v_offset <= grid_size; v_offset += square_size){
+    for(var h_offset = 0; h_offset <= grid_size; h_offset += square_size){
+        context = canvas.getContext("2d");
+        context.fillStyle = getRandomColor();
+        context.fillRect(h_offset, v_offset, square_size, square_size);
+    }
+}
