307eb1be872fc94fd83c6ae270a496c71d03c11d
Alfredo Gallegos <j.gallegos.chavarria@accenture.com>
2016-12-25 21:55:52 -0600

    triangle drawing behavior implemented

--- js/grid.js
+++ js/grid.js
@@ -14,7 +14,27 @@ canvas = document.getElementById("grid");
 for(var v_offset = 0; v_offset <= grid_size; v_offset += square_size){
     for(var h_offset = 0; h_offset <= grid_size; h_offset += square_size){
         context = canvas.getContext("2d");
+
+        //draw bottom part of the square
+        context.beginPath();
+        context.moveTo(h_offset, v_offset);
+
+        context.lineTo(h_offset, v_offset-square_size);
+        context.lineTo(h_offset + square_size, v_offset - square_size);
+        context.closePath();
+
         context.fillStyle = getRandomColor();
-        context.fillRect(h_offset, v_offset, square_size, square_size);
+        context.fill();
+
+        //draw bottom part of the square
+        context.beginPath();
+        context.moveTo(h_offset, v_offset);
+
+        context.lineTo(h_offset + square_size, v_offset);
+        context.lineTo(h_offset + square_size, v_offset - square_size);
+        context.closePath();
+
+        context.fillStyle = getRandomColor();
+        context.fill();
     }
 }
